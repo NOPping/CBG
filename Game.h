cd93758e9c9cfe4663841876f2c7ca2b4ab34e44
Ian Duffy <ian@ianduffy.ie>
2013-03-28 12:25:56 +0000

    Add spacing to Game header file.

--- Game.h
+++ Game.h
@@ -3,18 +3,19 @@
 
 class Game {
     private:
-    const int amountOfPlayers;
-    const int rows;
-    const int columns;
-    Square* grid[][];
-    Player* players[];
-    int currentPlayer;
-    int state;
+      const int amountOfPlayers;
+      const int rows;
+      const int columns;
+      Square* grid[][];
+      Player* players[];
+      int currentPlayer;
+      int state;
 
-    bool      getMove();
-    void      drawScreen();
+      bool      getMove();
+      void      drawScreen();
+    
     public:
-    virtual void Start();
+      virtual void Start();
 };
 
 #endif
