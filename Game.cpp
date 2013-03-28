752875ffd7c176f15f83c1888600aeb83e04f32b
Ian Duffy <ian@ianduffy.ie>
2013-03-28 12:24:25 +0000

    Fix if statement on start method and include the redraw of the screen

--- Game.cpp
+++ Game.cpp
@@ -2,7 +2,9 @@
 
 virtual void start() {
     while(state == 0) {
-        if(getMove());
-        currentPlayer = (currentPlayer == 0) ? 1 : 0;
+        if(getMove()) {
+          currentPlayer = (currentPlayer == 0) ? 1 : 0;
+        }
+        drawScreen();
     }
 }
