6c980201ad798696c8b9cf762c117503373182e2
Ian Duffy <ian@ianduffy.ie>
2013-03-31 21:58:50 +0100

    Add the menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+#include <iostream>
 #include "Checkers.h"
+#include "ConnectFour.h"
+
+using namespace std;
 
 int main() {
-  Checkers game = Checkers();
-  game.start();
+  int selection=0;
+  cout << "Welcome to compendium of board games" << "\n";
+  cout << "====================================" << "\n";
+  cout << "1) Checkers\n2) ConnectFour\n3) Snakes and Ladders\n4) Reversi\n";
+  cout << "====================================" << "\n";
+  cout << "\nInsert the number of the game you wish to play: ";
+  cin >> selection;
+  if(selection == 1) {
+    Checkers game = Checkers();
+    game.start();
+  } else {
+    cout << "Not yet made\n";
+  }
   return 0;
 }
