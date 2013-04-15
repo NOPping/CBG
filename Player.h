8f51cd5c7e987bb95189f428dc7dd12370b13b4d
Darren <darrenbrogan@hotmail.com>
2013-04-15 20:12:08 +0200

    Update spacing

--- Player.h
+++ Player.h
@@ -13,19 +13,19 @@ class Player {
     protected:
       ///Protected Data.
       ///Int to store the amount of pieces the player currently owns.
-      int             amountOfPieces;
+      int amountOfPieces;
       
       ///Int variable to store the max amount of pieces a player could own.
-      int             maxPieces;
+      int maxPieces;
       
       ///Variable used to store the amount of types of pieces the player has.
-      int             amountOfTypes;
+      int amountOfTypes;
      
       ///Vector to store the different types of pieces the player could own.
-      vector<string>  types;
+      vector<string> types;
       
       ///Vector of type Piece pointer to store all the pieces the player owns.
-      vector<Piece*>  pieces;
+      vector<Piece*> pieces;
 
     public:
       ///Public constructors.
@@ -34,24 +34,24 @@ class Player {
       virtual ~Player();
       
       ///Public functions.
-      int     getAmountOfPieces();
+      int getAmountOfPieces();
       
       ///Remove Piece from the player and return a bollean to identify 
       ///if it succeded or failed.
-      bool    removePiece();
+      bool removePiece();
       
       ///Check if pieces vector at index type is empty
-      bool    hasType(int type);
+      bool hasType(int type);
       
       ///Returns a string from pieces vector at index type. 
-      string  getCharacter(int type);
+      string getCharacter(int type);
       
       ///Add pieces to the pieces vector.
-      Piece*  addPiece();
-      Piece*  addPiece(Piece* insert);
+      Piece* addPiece();
+      Piece* addPiece(Piece* insert);
       
       ///Returns a pointer to the piece in the pieces vector at index index
-      Piece*  getPiece(int index);
+      Piece* getPiece(int index);
       
       ///Return weather this player can move or not
       bool suspended;
