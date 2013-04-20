b56db7e50b5a7278314188c09f04452e883df2c8
Ian Duffy <ian@ianduffy.ie>
2013-04-20 23:10:55 +0100

    Wrap systempiece to 80 characters

--- SystemPiece.cpp
+++ SystemPiece.cpp
@@ -4,8 +4,8 @@
 
 #include "SystemPiece.h"
 
-SystemPiece::SystemPiece(Player& owner, Coordinate source, Coordinate destination,
-                         int identifier)
+SystemPiece::SystemPiece(Player& owner, Coordinate source,
+                         Coordinate destination, int identifier)
 : Piece(owner), SourcePiece(owner, source),
 DestinationPiece(owner, destination), IdentifierPiece(owner, identifier) {
 }
