8ff5e00bba89e00e1cc2c730d7e4eb3ba58b9a87
morganp4 <petermorgan25@hotmail.com>
2013-04-30 15:14:03 +0200

    Add comments

--- Reversi.h
+++ Reversi.h
@@ -7,19 +7,33 @@
 #include <iostream>
 #include "Game.h"
 class Reversi:public Game {
+    //gets a move from the player
+    bool getMove();
 
-  bool getMove();
-  void drawScreen() const;
-  bool isValidMove(int destinationX) const;  
-  int  getPoint(string message, int range) const;
-  bool executeMove(Square& destinationSquare)const;
-  //bool executeMove(Square* sourceSquare,Square* destinationSquare,Square* toJump) const;
-  int  isOver() const;
-  int  getOpposition() const;
-  bool checkNext(Square &current,int yOffset,int xOffset,bool flip) const;
-  bool flanks(Square current,bool flip) const;
-  bool isLegal(Coordinate current,int iOffset,int jOffset) const;
-  bool checkLine(Coordinate current,int rowOffset,int columnOffSet,bool flip) const;
+    //flushes the screen and prints out the board
+    void drawScreen() const;
+
+    //determins if a destinaiton swuare is valid   
+    bool isValidMove(int destinationX) const;  
+
+    //Gets a single int from the user
+    int  getPoint(string message, int range) const;
+
+    // call the flanks method to ensure the destination coordinate  flanks an opponents piece
+    //if it does it adds the piece to the destination square
+    bool executeMove(Square& destinationSquare)const;
+    //determines the winner  of the game
+    int  isOver() const;
+    int  getOpposition() const;
+    //checks a line of pieces to see if it ends with a piece that belong to the 
+    //current player if it does, it flips the line of pieces to the current players colour   
+    bool checkNext(Square &current,int yOffset,int xOffset,bool flip) const;
+    //checks if a coordinate flanks an opponents piece 
+    //calls check next to see if the piece is inline with an existing piece owned 
+    //by the same player 
+    bool flanks(Square current,bool flip) const;
+    //checks if a move is  within the bounds of the board
+    bool isLegal(Coordinate current,int iOffset,int jOffset) const;
 public:
   Reversi();
 };
