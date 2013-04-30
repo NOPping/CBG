1b486336468d7fa774c2bad24fd14d9b7f012cfe
morganp4 <petermorgan25@hotmail.com>
2013-04-30 19:23:26 +0200

    Updates Comments

--- Reversi.h
+++ Reversi.h
@@ -15,26 +15,26 @@ class Reversi:public Base {
     /// Gets a move from the player
     bool getMove();
 
-    //determins if a destinaiton swuare is valid   
+    ///Determins if a destination square is valid   
     bool isValidMove(int destinationX) const;  
 
-    // call the flanks method to ensure the destination coordinate  flanks an opponents piece
-    //if it does it adds the piece to the destination square
+    /// Call the flanks method to ensure the destination coordinate  flanks an opponents piece
+    // If it does it adds the piece to the destination square
     bool executeMove(Square& destinationSquare)const;
     
-    //determines the winner  of the game
+    //Determines the winner of the game
     int  isOver() const;
     
-    //checks a line of pieces to see if it ends with a piece that belong to the 
-    //current player if it does, it flips the line of pieces to the current players colour   
+    ///Checks a line of pieces to see if it ends with a piece that belong to the 
+    ///current player if it does, it flips the line of pieces to the current players colour   
     bool checkNext(Square &current,int yOffset,int xOffset,bool flip) const;
     
-    //checks if a coordinate flanks an opponents piece 
-    //calls check next to see if the piece is inline with an existing piece owned 
-    //by the same player 
+    ///Checks if a coordinate flanks an opponents piece. 
+    ///Calls check next to see if the piece is inline with an existing piece owned 
+    ///by the same player. 
     bool flanks(Square current,bool flip) const;
     
-    //checks if a move is  within the bounds of the board
+    //Checks if a move is  within the bounds of the board.
     bool isLegal(Coordinate current,int iOffset,int jOffset) const;
     
 public:
